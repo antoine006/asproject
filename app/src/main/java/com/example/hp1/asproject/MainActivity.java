ef8c33ea79deab0ebeba815489d7b9030aca7f97
antoine006 <antoineshukha@gmail.com>
2018-10-08 13:38:12 +0300

    hi

--- app/src/main/java/com/example/hp1/asproject/MainActivity.java
+++ app/src/main/java/com/example/hp1/asproject/MainActivity.java
@@ -2,18 +2,12 @@ package com.example.hp1.asproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
- TextView tv1;
- EditText et1,et2;
- Button signin,signup;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
     }
 }
